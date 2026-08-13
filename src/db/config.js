836f1f111328130ca4b02b7ce3989be6508cb139
Julio Castro <cq.julio.antonny@gmail.com>
2026-08-13 17:35:18 -0500

    Proyecto en sus ultimos avances

--- src/db/config.js
+++ src/db/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-	connectionString: process.env.DATABASE_PUBLIC_URL,
+	connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
